c82d1adf214a0b41622d52c2040478dc0849ca15
Thaniell <thaniellfox@gmail.com>
2022-11-14 00:25:20 +0300

    minor cnanges in conftest and test_items

--- conftest.py
+++ conftest.py
@@ -31,5 +31,3 @@ def browser(request):
     print("\nquit browser..")
     browser.quit()
 
-
-
--- test_items.py
+++ test_items.py
@@ -1,5 +1,4 @@
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 import time
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
@@ -12,6 +11,9 @@ def is_element_present(browser):
     except:
         return False
 
-def test_guest_should_see_add_to_bin(browser):
+
+def test_guest_should_see_add_to_bin_button(browser):
     browser.get(link)
+    time.sleep(30)
     assert is_element_present(browser) == True, "No add to bin button on coders-at-work page"
+    
